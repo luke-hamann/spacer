d9b44e9d2a4bb55504f1bf0feb8a0f9e17b4a160
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2024-11-10 21:20:21 -0600

    Added styling

--- spacer/Areas/Account/Models/LoginForm.cs
+++ spacer/Areas/Account/Models/LoginForm.cs
@@ -4,10 +4,10 @@ namespace spacer.Areas.Account.Models
 {
     public class LoginForm
     {
-        [Required]
+        [Required(ErrorMessage = "Please enter a Username/Email.")]
         public string? Identity { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a Password.")]
         public string? Password { get; set; }
     }
 }
