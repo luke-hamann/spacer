132a3346c366b1c233d67b4841abfccf7db093f6
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-15 10:08:41 -0600

    Improve home controller; Fix search

--- spacer/Controllers/HomeController.cs
+++ spacer/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace spacer.Controllers
             ViewBag.currentUser = GetCurrentUser();
             ViewBag.popularSubspaces = GetPopularSubspaces();
 
-            var posts = _context.Posts
+            List<Post> posts = _context.Posts
                 .Include(p => p.Subspace)
                 .Include(p => p.User)
                 .OrderByDescending(s => s.creationDate)
@@ -49,20 +49,23 @@ namespace spacer.Controllers
         {
             ViewBag.currentUser = GetCurrentUser();
             ViewBag.popularSubspaces = GetPopularSubspaces();
-            ViewBag.subspace = _context.Subspaces.Where(s => s.name == name).FirstOrDefault();
+
+            ViewBag.subspace = _context.Subspaces
+                .Where(s => s.name == name)
+                .FirstOrDefault();
 
             if (ViewBag.subspace == null)
             {
                 return NotFound();
             }
 
-            int id = ViewBag.subspace.id;
-            ViewBag.searchSubspaceId = id;
+            int subspaceId = ViewBag.subspace.id;
+            ViewBag.searchSubspaceId = subspaceId;
 
             var query = _context.Posts
                 .Include(p => p.User)
                 .Include(p => p.Subspace)
-                .Where(p => p.Subspace!.id == id);
+                .Where(p => p.Subspace!.id == subspaceId);
             
             if (sort == "newest")
             {
@@ -85,14 +88,16 @@ namespace spacer.Controllers
             ViewBag.currentUser = GetCurrentUser();
             ViewBag.popularSubspaces = GetPopularSubspaces();
 
-            ViewBag.userProfile = _context.Users.Where(u => u.name == name).FirstOrDefault();
+            ViewBag.userProfile = _context.Users
+                .Where(u => u.name == name)
+                .FirstOrDefault();
 
             if (ViewBag.userProfile == null)
             {
                 return NotFound();
             }
 
-            int id = ViewBag.userProfile.id;
+            int userId = ViewBag.userProfile.id;
 
             if (section == "posts")
             {
@@ -101,17 +106,18 @@ namespace spacer.Controllers
                 ViewBag.content = _context.Posts
                     .Include(p => p.Subspace)
                     .Include(p => p.User)
-                    .Where(p => p.userId == id)
+                    .Where(p => p.userId == userId)
                     .OrderBy(p => p.creationDate)
                     .ToList();
-            } else
+            }
+            else
             {
                 ViewBag.selection = "comments";
 
                 ViewBag.content = _context.Comments
                     .Include(p => p.Post)
                     .Include(p => p.User)
-                    .Where(p => p.userId == id)
+                    .Where(p => p.userId == userId)
                     .ToList();
             }
 
@@ -124,15 +130,16 @@ namespace spacer.Controllers
         {
             ViewBag.currentUser = GetCurrentUser();
             ViewBag.popularSubspaces = GetPopularSubspaces();
+
             ViewBag.searchQuery = query;
             ViewBag.searchSubspaceId = subspaceId;
-
 
             List<Post> posts = _context.Posts
                 .Include(p => p.User)
                 .Include(p => p.Subspace)
-                .Where(p => (query == "" || p.title.Contains(query) || p.content.Contains(query)))
-                .Where(p => subspaceId == 0 || p.Subspace!.id == subspaceId)
+                .Where(p => (query == null || p.title.Contains(query) || p.content.Contains(query)))
+                .Where(p => (subspaceId == 0 || p.Subspace!.id == subspaceId))
+                .OrderByDescending(p => p.creationDate)
                 .ToList();
 
             return View(posts);
